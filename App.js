dd18baab29c715352987f2ca0f16d604c8c80455
Pasansi Imanya <pasansiimanya16@gmail.com>
2023-05-20 18:34:23 +0530

    modifications

--- App.js
+++ App.js
@@ -2,19 +2,21 @@ import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SignupScreen from "./src/screens/SignupScreen";
 import SigninScreen from "./src/screens/SigninScreen";
-import MainScreen from "./src/screens/MainScreen";
-import DonationBoard from "./src/screens/DonationBoard";
-import SponsorScreen from "./src/screens/SponsorScreen";
+// import MainScreen from "./src/screens/MainScreen";
+// import DonationBoard from "./src/screens/DonationBoard";
+// import SponsorScreen from "./src/screens/SponsorScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
-import Donation from "./src/screens/Donation/Donation";
-import DonateAmount from "./src/screens/DonateAmount";
-import SponsorAmount from "./src/screens/SponsorAmount";
-import RecurringScreen from "./src/screens/RecurringScreen";
+// import Donation from "./src/screens/Donation/Donation";
+// import DonateAmount from "./src/screens/DonateAmount";
+// import SponsorAmount from "./src/screens/SponsorAmount";
+// import RecurringScreen from "./src/screens/RecurringScreen";
+import HomeScrenn from "./src/screens/HomeScreen/HomeScrenn";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import AuthContextProvider from "./src/context/AuthContext";
 import PaymentContextProvider from "./src/context/PaymentContext";
 
 // Stripe.setOptionsAsync({
@@ -29,22 +31,18 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <StripeProvider publishableKey="pk_test_51N6SokEyrZyjTHe0PcPSXjZLwXWjD3lFXpHhyS5YZNbilUmZ7uP9VXOHVXDxqfOUvC9pdBcpA9A3nsrgQnsCdXsa00kHybSWpm">
+      <AuthContextProvider>
       <PaymentContextProvider>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="SigninScreen" component={SigninScreen} />
-            <Stack.Screen name="DonationBoard" component={DonationBoard} />
-            <Stack.Screen name="Donation" component={Donation} />
-            <Stack.Screen name="MainScreen" component={MainScreen} />
-            <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-            <Stack.Screen name="SignupScreen" component={SignupScreen} />
-            <Stack.Screen name="SponsorScreen" component={SponsorScreen} />
-            <Stack.Screen name="DonateAmount" component={DonateAmount} />
-            <Stack.Screen name="SponsorAmount" component={SponsorAmount} />
-            <Stack.Screen name="RecurringScreen" component={RecurringScreen} />
+          <Stack.Screen name="SigninScreen" component={SigninScreen} />
+              <Stack.Screen name="MainScreen" component={HomeScrenn} />
+              <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+              <Stack.Screen name="SignupScreen" component={SignupScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </PaymentContextProvider>
+      </AuthContextProvider>
     </StripeProvider>
   );
 }
